a1da4f3217b65c3ca3cd6221e843d1992fe7349a
Zero-G-Sys <74690685+Zero-G-Sys@users.noreply.github.com>
2022-10-12 21:08:42 -0300

    Update Volume Control to v1.1

--- VX/Volume Control.rb
+++ VX/Volume Control.rb
@@ -9,22 +9,21 @@
  and ME. Alternatevly you can use game variables to set audio volumes, you must
  initialize the volume values when you start the game, else it will read 0
  volume.
- 
- WARNING: Known bug, restarting game with F12 crashes the game
 
  == Terms of Use ==
  - Free for use in non-commercial projects.
  - Free for use in commercial projects.
- - Please provide credits to Zero_G and ‚Þ‚Á‚­Ru (Rutan)
+ - Please provide credits to Zero_G and ï¿½Þ‚ï¿½ï¿½ï¿½Ru (Rutan)
 
  == Credits ==
- Based on Volume menu plugin from ‚Þ‚Á‚­Ru (Rutan) 
+ Based on Volume menu plugin from ï¿½Þ‚ï¿½ï¿½ï¿½Ru (Rutan) 
 
  == Usage ==
  Just add the plugin before main. If using variables, remember to set them a
  value when starting a new game.
  
  == Changelog ==
+ v1.1 Fixed a crash when restarting game (Stack too deep on aliases)
  v1.0 Initial
 --------------------------------------------------------------------------------
 =end
@@ -45,9 +44,9 @@ end
 
 class << Audio
   #-----------------------------------------------------------------------------
-  # œ Playback: BGM
+  # ï¿½ï¿½ Playback: BGM
   #-----------------------------------------------------------------------------
-  alias hzm_vxa_audioVol_bgm_play bgm_play
+  alias hzm_vxa_audioVol_bgm_play bgm_play unless $@
   def bgm_play(filename, volume = 100, pitch = 100)
     bgm_vol = ZERO::BGM_VOLUME
     bgm_vol = $game_variables[ZERO::BGM_VARIABLE] if ZERO::BGM_VARIABLE != 0
@@ -55,9 +54,9 @@ class << Audio
     hzm_vxa_audioVol_bgm_play(filename, bgm_vol * volume / 100, pitch)
   end
   #-----------------------------------------------------------------------------
-  # œ Playback: BGS
+  # ï¿½ï¿½ Playback: BGS
   #-----------------------------------------------------------------------------
-  alias hzm_vxa_audioVol_bgs_play bgs_play
+  alias hzm_vxa_audioVol_bgs_play bgs_play unless $@
   def bgs_play(filename, volume = 100, pitch = 100)
     bgs_vol = ZERO::BGS_VOLUME
     bgs_vol = $game_variables[ZERO::BGS_VARIABLE] if ZERO::BGS_VARIABLE != 0
@@ -65,9 +64,9 @@ class << Audio
     hzm_vxa_audioVol_bgs_play(filename,bgs_vol * volume / 100, pitch)
   end
   #-----------------------------------------------------------------------------
-  # œ Playback: SE
+  # ï¿½ï¿½ Playback: SE
   #-----------------------------------------------------------------------------
-  alias hzm_vxa_audioVol_se_play se_play
+  alias hzm_vxa_audioVol_se_play se_play unless $@
   def se_play(filename, volume = 100, pitch = 100)
     se_vol = ZERO::SE_VOLUME
     se_vol = $game_variables[ZERO::SE_VARIABLE] if ZERO::SE_VARIABLE != 0
@@ -75,9 +74,9 @@ class << Audio
     hzm_vxa_audioVol_se_play(filename, se_vol * volume / 100, pitch)
   end
   #-----------------------------------------------------------------------------
-  # œ Playback: ME
+  # ï¿½ï¿½ Playback: ME
   #-----------------------------------------------------------------------------
-  alias hzm_vxa_audioVol_me_play me_play
+  alias hzm_vxa_audioVol_me_play me_play unless $@
   def me_play(filename, volume = 100, pitch = 100)
     me_vol = ZERO::ME_VOLUME
     me_vol = $game_variables[ZERO::ME_VARIABLE] if ZERO::ME_VARIABLE != 0
